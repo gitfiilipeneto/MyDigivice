0444737f9a57a0059108efe56c35ec9220219d4f
Filipe Neto <filipenetopires@hotmail.com>
2020-12-23 15:57:02 -0300

    hero start

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+width:100vw;
+height:100vh;
+max-widht:100%;
+display:flex;
+justify-content: center;
+align-items: center;
+`
+
+const styledH1 = styled.h1`
+font-size: 96px;
+
+`
+const HeroSection = () => {
+    return(
+        <StyledContainer>
+            <styledH1>Oi</styledH1>
+        </StyledContainer>
+    )
+}
+
+export default HeroSection
